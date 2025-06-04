860e35c27a6437a46b9e926aac53101fecf7e48f
WANI-OWAIS <waniowais528@gmail.com>
2025-06-04 19:41:35 +0530

    02COMPLETED

---
+++ 02/script.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+// fs.writeFile("hey.txt", "Hello world", function(err){
+//     if(err) console.log(err);
+//     else console.log("File created successfully!");
+// })
+
+// fs.appendFile("hey.txt", "Hello How are You", function(err){
+//     if(err) console.log(err);
+//     else console.log("Added successfully!");
+// })
+
+// fs.rename("hey.txt", "hello.txt", function(err){
+//     if(err) console.log(err);
+//     else console.log("File renamed successfully!");
+// })
+
+// fs.copyFile("hello.txt", "./copy/copy.txt", function(err){
+//     if(err) console.log(err);
+//     else console.log("File copied successfully!");
+// })
+
+// fs.unlink("hello.txt", function(err){
+//     if(err) console.log(err);
+//     else console.log("File deleted successfully!");
+// })
+
+// fs.rm("./test", {recursive: true}, function(err){
+//     if(err) console.log(err);
+//     else console.log("Directory deleted successfully!");
+// })
+
+
+// fs.readFile("hello.txt", "utf-8", function(err, data){
+//     if(err) console.log(err);
+//     else{ console.log("File read successfully!");
+//     console.log("Content:\n" + data);
+//     }
+// })
+
+//HTTP protocol
+const http = require('http');
+const server = http.createServer(function(req, res){
+  res.end("Hello from the server!");
+})
+server.listen(3000);

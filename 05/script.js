ea01712320e9b549c291cd91ff58a8ab7115863f
WANI-OWAIS <waniowais528@gmail.com>
2025-06-11 16:47:15 +0530

    05Completed

---
+++ 05/script.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(function(req, res, next){
+    console.log("Middleware chala");
+    next();
+})
+
+app.get("/", function(req, res) {
+    res.send("Hello World")
+});
+
+app.get("/profile", function(req, res, next){
+    return next(new Error("Profile not found"));
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send('Something broke!')
+})
+
+
+app.listen(3000)
